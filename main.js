303a90faf9a772ca17e57400bc105ba299550365
Aoife Tjärnlund <tjarnlunda@gmail.com>
2021-05-22 10:27:41 +0200

    lite ändringar mvh if

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++){
     var span = document.createElement("SPAN");
-    var txt = document.cgitreateTextNode("\u00d7");
+    var txt = document.createTextNode("\u00d7");
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
